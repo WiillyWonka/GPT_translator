e35fdbe85f28edc32a16429851e0401a04189251
Rizhenko Viktor <rizhenko.viktor.ww@gmail.com>
2025-01-11 19:50:51 +0300

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-uvicorn app.main:app --host 0.0.0.0 --port 80
+if [ "$FRONTEND" = "true" ]; then
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+    streamlit run frontend/main.py --server.port 8502 --server.address 0.0.0.0 &
+else
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 &
+fi
+
+wait
